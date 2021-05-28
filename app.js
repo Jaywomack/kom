cd1e26ee3352a03b02743f5bf24beaaf13f87f6b
jaywomack <womackjw@hotmail.com>
2021-05-27 17:42:30 -0700

    Boostrapped about and blogHome

--- app.js
+++ app.js
@@ -6,10 +6,24 @@ require('dotenv').config();
 const app = express();
 const port = 3000;
 app.set('view engine', 'ejs');
+express.static(__dirname);
+app.use(express.static('public'));
 
 mongoose.connect(process.env.HOST, {
   useNewUrlParser: true,
   useUnifiedTopology: true
+});
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/blog', (req, res) => {
+  res.render('blogHome');
 });
 
 app.get('/create', (req, res) => {
